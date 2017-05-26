f4efcdbe5ceedda7b09881c597444d60c1f0f73d
Shinji Kato <hole.in1.kato@gmail.com>
2017-05-26 14:08:47 +0630

    added python3 version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hmyau99
 simple program sample. show 9x9 multiplication result
+
+hmyau.py:  Python2
+hmyau3.py: Python3
--- hmyau.py
+++ hmyau.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+# Python2 version. does not work on Python3
 for row in range(1,10):
     for col in range(1,10):
         print '%dx%d=%2d ' %(col,row,row*col),
