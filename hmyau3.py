69808f0a10bed7bdab46375799be3aec83f358a4
Shinji Kato <hole.in1.kato@gmail.com>
2017-06-01 10:39:33 +0630

    clean up comments

--- hmyau3.py
+++ hmyau3.py
@@ -3,9 +3,6 @@
 
 for row in range(1,10):
     for col in range(1,10):
-#       print (str(col)+"x"+str(row)+"="+str(row*col), end=" ")
-#       print ("%dx%d=%2d" %(col,row,row*col), end=" ")
-#       print (str(col)+"x"+str(row)+"="+"{0:2d}".format(row*col), end=" ")
         print ("{0:d}x{1:d}={2:2d}".format(col,row,row*col), end=" ")
     print()
 
