9c2a0e5735dcf5966131cb081bc0e9791a42d94e
Shinji Kato <hole.in1.kato@gmail.com>
2017-06-01 10:56:10 +0630

    remove last blank

--- hmyau3.py
+++ hmyau3.py
@@ -2,10 +2,10 @@
 #-*- coding: utf-8 -*-
 
 for row in range(1,10):
-    for col in range(1,10):
+    for col in range(1,9):
 #       print (str(col)+"x"+str(row)+"="+str(row*col), end=" ")
 #       print ("%dx%d=%2d" %(col,row,row*col), end=" ")
 #       print (str(col)+"x"+str(row)+"="+"{0:2d}".format(row*col), end=" ")
         print ("{0:d}x{1:d}={2:2d}".format(col,row,row*col), end=" ")
-    print()
+    print ("{0:d}x{1:d}={2:2d}".format(9,row,row*9))
 
