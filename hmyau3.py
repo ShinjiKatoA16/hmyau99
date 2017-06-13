f31b257c07c32dfd8c29e494e95d247fe630606b
Shinji Kato <hole.in1.kato@gmail.com>
2017-06-13 14:25:44 +0630

    Brush up format string

--- hmyau3.py
+++ hmyau3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
+output_str = "{0:d}x{1:d}={2:2d}"
+
 for row in range(1,10):
     for col in range(1,9):
-        print ("{0:d}x{1:d}={2:2d}".format(col,row,row*col), end=" ")
-    print ("{0:d}x{1:d}={2:2d}".format(9,row,row*9))
+        print (output_str.format(col,row,row*col), end=" ")
+    print (output_str.format(9,row,row*9))
 
