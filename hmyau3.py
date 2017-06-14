1cb0c6d374f716d34d8db6fde4bacde0d08b0b47
Shinji Kato <hole.in1.kato@gmail.com>
2017-06-14 09:41:52 +0630

    Variation-0: 1 Trailing space

--- hmyau3.py
+++ hmyau3.py
@@ -6,8 +6,10 @@ FORMULA_STR = "{0:d}x{1:d}={2:2d}"
 MIN_NUM = 1
 MAX_NUM = 9
 
+
+# Variation-0: 1 extra trailing space
 for row in range(MIN_NUM,MAX_NUM+1):
-    for col in range(MIN_NUM,MAX_NUM):
-        print (FORMULA_STR.format(col,row,row*col), end=" ")
-    print (FORMULA_STR.format(MAX_NUM,row,row*MAX_NUM))
+    for col in range(MIN_NUM,MAX_NUM+1):
+        print(FORMULA_STR.format(col,row,row*col), end=" ")
+    print()
 
