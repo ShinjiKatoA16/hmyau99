5d168e7aa4de9f9578f1c52d98653eef96c24d7d
Shinji Kato <hole.in1.kato@gmail.com>
2017-06-30 14:53:10 +0630

    Add docstring and Change variable name to terminator

--- hmyau3.py
+++ hmyau3.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+'''
+Python3 sample program to display 1x1 - 9x9 multiplier result
+2017/Jun/30 Shinji Kato (github: ShinjiKatoA16)
+'''
 
 # constant definition
 FORMULA_STR = "{0:d}x{1:d}={2:2d}"
@@ -8,7 +13,7 @@ MAX_NUM = 9
 
 for row in range(MIN_NUM,MAX_NUM+1):
     for col in range(MIN_NUM,MAX_NUM+1):
-        if col != MAX_NUM: separator = ' '
-        else: separator = '\n'
-        print (FORMULA_STR.format(col,row,row*col), end=separator)
+        if col != MAX_NUM: terminator = ' '
+        else: terminator = '\n'
+        print (FORMULA_STR.format(col,row,row*col), end=terminator)
 
