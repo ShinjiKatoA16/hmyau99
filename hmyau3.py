0fecbf849265de852d9d12296ed6a47e1eb0981e
Shinji Kato <hole.in1.kato@gmail.com>
2017-11-13 08:26:29 +0630

    join method version

--- hmyau3.py
+++ hmyau3.py
@@ -3,7 +3,7 @@
 
 '''
 Python3 sample program to display 1x1 - 9x9 multiplier result
-2017/Jun/30 Shinji Kato (github: ShinjiKatoA16)
+2017/Nov/13 Shinji Kato (github: ShinjiKatoA16)
 '''
 
 # constant definition
@@ -12,8 +12,8 @@ MIN_NUM = 1
 MAX_NUM = 9
 
 for row in range(MIN_NUM,MAX_NUM+1):
+    output = list()
     for col in range(MIN_NUM,MAX_NUM+1):
-        if col != MAX_NUM: terminator = ' '
-        else: terminator = '\n'
-        print (FORMULA_STR.format(col,row,row*col), end=terminator)
+        output.append(FORMULA_STR.format(col,row,row*col))
+    print(' '.join(output))
 
